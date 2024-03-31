044faf38e70276eb5e0e17d2cacada59f3cd5f51
mahammushtaqdev <mahammushtaq80@gmail.com>
2024-03-31 15:30:51 +0500

    my first commit

---
+++ 31 assignment/index.ts
@@ -0,0 +1,43 @@
+//operators
+//addition
+//comparation
+//logical
+//assignment
+//addition
+
+let x=2;
+let y =3;
+
+let add ="x "+ "y"; //7
+let add1 ="x" + "y";//xy
+let add2 ="xy + x+y"//xy25
+let add3 ="xy" + add;//xy7
+let add4 = x + y + "xy";//7xy
+let add5 = x+y +"xy" + x ; //7xy2
+let add6 = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//division
+
+
