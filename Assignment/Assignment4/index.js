66d678666275a6076e1c946dd2fae30b8b5f2444
mahammushtaqdev <mahammushtaq80@gmail.com>
2024-04-17 16:58:50 +0500

    assignment

--- Assignment/Assignment4/index.js
+++ Assignment/Assignment4/index.js
@@ -23,3 +23,23 @@ let message; //Declared a variable and annotated its type string.
 //message = 5; after giving message string  value we cannot change it
 //Problem #5
 console.log(`Remainder of ${num2} and ${num1} is: ${num2 % num1}`);
+//Problem #6 Increment Challenge
+let counter = 0; // value of counter here is 0
+counter = counter + 1; // value of counter here is 1
+counter += 1; // value of counter here is 2
+counter++; // value of counter here is 3
+//Problem #7 boolean operators
+let aa = true;
+let bb = false;
+let cc = true;
+aa && bb && cc; // used AND operator expression 
+aa || bb || cc; // used OR operator expression
+!aa;
+!bb;
+!cc; // used NOT gate expression
+//Problem #8 Compound Assignment operators
+let num = 10;
+num += 1; // num = num + 1; (11)
+num -= 1; // num = num - 1; (10)
+num /= 1; // num = num / 1; (10)
+num *= 1; // num = num * 1; (10)
